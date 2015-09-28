f3a034c2b1f423a1903b2c064b1a50ba2fc2e3ac
Sindre Sorhus <sindresorhus@gmail.com>
2015-09-29 02:48:00 +0700

    bump deps

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,8 @@ gulp.task('static', function () {
   .pipe(jshint.reporter('jshint-stylish'))
   .pipe(jshint.reporter('fail'))
   .pipe(jscs())
+  .pipe(jscs.reporter())
+  .pipe(jscs.reporter('fail'))
   .on('error', gutil.log);
 });
 
