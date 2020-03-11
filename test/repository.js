13505a4384a0775954312c2405829e93702a8307
Marcelo Boveto Shima <marceloshima@gmail.com>
2020-03-11 00:40:31 -0300

    Implement repository and createEnvWithVersion

---
+++ test/repository.js
@@ -0,0 +1,64 @@
+'use strict';
+const assert = require('assert');
+const fs = require('fs-extra');
+const path = require('path');
+
+const repository = require('../lib/util/repository');
+const Env = require('..');
+
+describe('repository', () => {
+  afterEach(function () {
+    this.timeout(20000);
+    fs.removeSync(repository.repositoryPath);
+    repository.cleanupPackageCache('yeoman-environment', true);
+  });
+
+  it('is exposed on the Environment object', () => {
+    assert.equal(Env.repository, repository);
+  });
+
+  describe('#repositoryPath', () => {
+    it('returns the repository path', () => {
+      assert.equal(repository.repositoryPath, path.resolve('.yo-repository'));
+    });
+  });
+
+  describe('#resolvePackagePath', () => {
+    describe('without args', () => {
+      it('returns the node_modules path', () => {
+        assert.equal(repository.nodeModulesPath, repository.resolvePackagePath());
+      });
+    });
+
+    describe('with args', () => {
+      it('returns the module path', () => {
+        assert.equal(path.join(repository.nodeModulesPath, 'package', 'module'), repository.resolvePackagePath('package', 'module'));
+      });
+    });
+  });
+
+  describe('#createEnvWithVersion()', () => {
+    it('returns env', function () {
+      this.timeout(500000);
+      const env = Env.createEnvWithVersion('2.3.0');
+      assert.ok(env);
+      assert.ok(!(env instanceof Env));
+    });
+  });
+
+  describe('repository workflow', () => {
+    it('run the workflow correctly', function () {
+      this.timeout(500000);
+      repository.installPackage('yeoman-environment', '2.3.0');
+      assert.equal(repository.getPackageVersion('yeoman-environment'), '2.3.0');
+
+      // Force install another version
+      repository.installPackage('yeoman-environment', '2.8.1');
+      assert.equal(repository.getPackageVersion('yeoman-environment'), '2.8.1');
+
+      // Instantiate the installed version
+      assert.ok(repository.requireModule('yeoman-environment'));
+      assert.equal(repository.getPackageVersion('yeoman-environment'), '2.8.1');
+    });
+  });
+});
