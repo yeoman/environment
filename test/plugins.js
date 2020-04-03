158813ac4977e5326c939044c98e956cbe2a2be4
Marcelo Shima <marceloshima@gmail.com>
2020-04-03 00:30:59 -0300

    Add support to dependency-less generators. (#193)
    * Implement dependency-less generators.
    
    * Change yeoman-generator dependency from dev to prod

---
+++ test/plugins.js
@@ -0,0 +1,62 @@
+'use strict';
+const assert = require('assert');
+const os = require('os');
+const path = require('path');
+const fs = require('fs-extra');
+const Environment = require('../lib/environment');
+
+const tmpdir = path.join(os.tmpdir(), 'yeoman-environment/light');
+
+describe('Generators plugin', () => {
+  before(function () {
+    fs.mkdirpSync(tmpdir);
+    this.cwd = process.cwd();
+    process.chdir(tmpdir);
+    if (fs.existsSync(Environment.repository.repositoryPath)) {
+      fs.removeSync(Environment.repository.repositoryPath);
+    }
+  });
+
+  after(function () {
+    process.chdir(this.cwd);
+    fs.removeSync(tmpdir);
+  });
+
+  [undefined, '4.7.2', 'super:app'].forEach(extended => {
+    describe('#run', () => {
+      beforeEach(function () {
+        this.timeout(300000);
+        delete this.execValue;
+
+        this.env = new Environment([], {'skip-install': true, experimental: true});
+
+        const self = this;
+        const superGenerator = {createGenerator(env) {
+          return class extends env.requireGenerator(undefined) {
+            exec() {}
+          };
+        }};
+        this.env.registerStub(superGenerator, 'super:app');
+
+        const dummy = {createGenerator(env) {
+          return class extends env.requireGenerator(extended) {
+            exec() {
+              self.execValue = 'done';
+            }
+          };
+        }};
+        this.env.registerStub(dummy, 'dummy:app');
+        // Pre load the required generator
+        this.env.requireGenerator(extended);
+      });
+
+      it(`runs generators plugin with requireGenerator value ${extended}`, function () {
+        this.timeout(100000);
+        const self = this;
+        return this.env.run('dummy:app').then(() => {
+          assert.equal(self.execValue, 'done');
+        });
+      });
+    });
+  });
+});
