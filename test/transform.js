6923fca1642824e7fc2cbe5c594f38323437a115
Marcelo Boveto Shima <marceloshima@gmail.com>
2021-02-15 00:04:14 -0300

    Add test for getConflicterStatusForFile

--- test/transform.js
+++ test/transform.js
@@ -2,10 +2,12 @@
 /* eslint-disable max-nested-callbacks, eslint-comments/disable-enable-pair */
 
 const assert = require('assert');
+const path = require('path');
 const through = require('through2');
 const {pipeline} = require('stream');
 const {
   fileIsModified,
+  getConflicterStatusForFile,
   createEachFileTransform,
   createYoRcTransform,
   createConflicterStatusTransform
@@ -223,6 +225,36 @@ describe('Transform stream', () => {
         assert.equal(file.conflicterChanges, undefined);
         assert.equal(file.conflicterLog, undefined);
       });
+    });
+  });
+
+  describe('getConflicterStatusForFile()', () => {
+    const yoResolveRoot = path.join(__dirname, 'fixtures', 'yo-resolve');
+    const yoResolveSub = path.join(yoResolveRoot, 'sub');
+    const rootToSkipFile = path.join(yoResolveRoot, 'root-to-skip');
+    const subToSkipFile = path.join(yoResolveSub, 'sub-to-skip');
+    const sub2ToForceFile = path.join(yoResolveSub, 'sub2-to-force');
+    const noResolveFile = path.join(yoResolveSub, 'no-resolve');
+    const matchToSkipFile = path.join(yoResolveSub, 'match-to-skip');
+
+    it('should return correct status for root-to-skip', () => {
+      assert.strictEqual(getConflicterStatusForFile({}, rootToSkipFile), 'skip');
+    });
+
+    it('should return correct status for sub-to-skip', () => {
+      assert.strictEqual(getConflicterStatusForFile({}, subToSkipFile), 'skip');
+    });
+
+    it('should return correct status for sub2-to-force', () => {
+      assert.strictEqual(getConflicterStatusForFile({}, sub2ToForceFile), 'force');
+    });
+
+    it('should return correct status for no-resolve', () => {
+      assert.strictEqual(getConflicterStatusForFile({}, noResolveFile), undefined);
+    });
+
+    it('should return correct status for match-to-skip', () => {
+      assert.strictEqual(getConflicterStatusForFile({}, matchToSkipFile), 'skip');
     });
   });
 });
