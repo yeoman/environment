ab9582a70073203c7a6b58fb6dbf1f4eba249d48
Marcelo Boveto Shima <marceloshima@gmail.com>
2021-02-14 21:14:45 -0300

    Implement packageManagerInstallTask.

---
+++ test/package-manager.js
@@ -0,0 +1,206 @@
+'use strict';
+/* eslint-disable max-nested-callbacks */
+const assert = require('assert');
+const path = require('path');
+const sinon = require('sinon');
+
+const PackageManager = require('../lib/package-manager')(class {});
+
+describe('environment (package-manager)', () => {
+  let packageManager;
+
+  beforeEach(() => {
+    packageManager = new PackageManager();
+    packageManager.adapter = {
+      log: sinon.stub()
+    };
+    packageManager.spawnCommand = sinon.stub().returns(Promise.resolve());
+    packageManager.options = {};
+    packageManager.sharedFs = {get: sinon.stub().returns()};
+    packageManager.cwd = path.join(__dirname, 'fixtures', 'package-manager', 'npm');
+  });
+
+  afterEach(() => {
+    delete packageManager.compatibilityMode;
+  });
+
+  describe('#packageManagerInstallTask()', () => {
+    describe('without a package.json', async () => {
+      beforeEach(() => {
+        return packageManager.packageManagerInstallTask();
+      });
+
+      it('should not log', () => {
+        assert(packageManager.adapter.log.notCalled);
+      });
+
+      it('should not call spawnCommand', () => {
+        assert(packageManager.spawnCommand.notCalled);
+      });
+    });
+
+    describe('with a package.json', async () => {
+      beforeEach(() => {
+        packageManager.sharedFs.get = sinon.stub().returns({});
+      });
+      describe('running generator < 5', async () => {
+        beforeEach(() => {
+          packageManager.compatibilityMode = 'v4';
+          return packageManager.packageManagerInstallTask();
+        });
+
+        it('should log', () => {
+          assert(packageManager.adapter.log.calledOnce);
+          assert(packageManager.adapter.log.getCall(0).calledWith(`
+Running in generator < 5 compatibility. Package manager install is done by the generator.
+`));
+        });
+
+        it('should not call spawnCommand', () => {
+          assert(packageManager.spawnCommand.notCalled);
+        });
+      });
+
+      describe('when package.json was not committed', () => {
+        beforeEach(async () => {
+          return packageManager.packageManagerInstallTask();
+        });
+
+        it('should log', () => {
+          assert(packageManager.adapter.log.calledOnce);
+          assert(packageManager.adapter.log.getCall(0).calledWith(`
+No change to package.json was detected. No package manager install will be executed.
+`));
+        });
+
+        it('should not call spawnCommand', () => {
+          assert(packageManager.spawnCommand.notCalled);
+        });
+      });
+
+      describe('when package.json was committed', () => {
+        beforeEach(async () => {
+          packageManager.sharedFs.get = sinon.stub().returns({committed: true});
+        });
+
+        describe('with skipInstall', () => {
+          beforeEach(async () => {
+            packageManager.options = {skipInstall: true};
+            await packageManager.packageManagerInstallTask();
+          });
+
+          it('should log', async () => {
+            assert(packageManager.adapter.log.calledTwice);
+            assert(packageManager.adapter.log.getCall(0).calledWith(`
+Changes to package.json were detected.`));
+            assert(packageManager.adapter.log.getCall(1).calledWith(`Skipping package manager install.
+`));
+          });
+
+          it('should not call spawnCommand', () => {
+            assert(packageManager.spawnCommand.notCalled);
+          });
+        });
+
+        describe('with npm', () => {
+          beforeEach(async () => {
+            packageManager.cwd = path.join(__dirname, 'fixtures', 'package-manager', 'npm');
+            await packageManager.packageManagerInstallTask();
+          });
+
+          it('should log', async () => {
+            assert(packageManager.adapter.log.calledTwice);
+            assert(packageManager.adapter.log.getCall(0).calledWith(`
+Changes to package.json were detected.`));
+            assert(packageManager.adapter.log.getCall(1).calledWith(`Running npm install for you to install the required dependencies.
+`));
+          });
+
+          it('should execute npm', () => {
+            assert(packageManager.spawnCommand.calledOnce);
+            assert(packageManager.spawnCommand.getCall(0).calledWith('npm', ['install']));
+          });
+        });
+
+        describe('with yarn', () => {
+          beforeEach(async () => {
+            packageManager.cwd = path.join(__dirname, 'fixtures', 'package-manager', 'yarn');
+            await packageManager.packageManagerInstallTask();
+          });
+
+          it('should log', async () => {
+            assert(packageManager.adapter.log.calledTwice);
+            assert(packageManager.adapter.log.getCall(0).calledWith(`
+Changes to package.json were detected.`));
+            assert(packageManager.adapter.log.getCall(1).calledWith(`Running yarn install for you to install the required dependencies.
+`));
+          });
+
+          it('should execute yarn', () => {
+            assert(packageManager.spawnCommand.calledOnce);
+            assert(packageManager.spawnCommand.getCall(0).calledWith('yarn', ['install']));
+          });
+        });
+
+        describe('with pnpm', () => {
+          beforeEach(async () => {
+            packageManager.cwd = path.join(__dirname, 'fixtures', 'package-manager', 'pnpm');
+            await packageManager.packageManagerInstallTask();
+          });
+
+          it('should log', async () => {
+            assert(packageManager.adapter.log.calledTwice);
+            assert(packageManager.adapter.log.getCall(0).calledWith(`
+Changes to package.json were detected.`));
+            assert(packageManager.adapter.log.getCall(1).calledWith(`Running pnpm install for you to install the required dependencies.
+`));
+          });
+
+          it('should execute pnpm', () => {
+            assert(packageManager.spawnCommand.calledOnce);
+            assert(packageManager.spawnCommand.getCall(0).calledWith('pnpm', ['install']));
+          });
+        });
+
+        describe('with an unsupported package manager', () => {
+          beforeEach(async () => {
+            packageManager.cwd = path.join(__dirname, 'fixtures', 'package-manager', 'npm');
+            packageManager.options = {nodePackageManager: 'foo'};
+            await packageManager.packageManagerInstallTask();
+          });
+
+          it('should log', async () => {
+            assert(packageManager.adapter.log.calledTwice);
+            assert(packageManager.adapter.log.getCall(0).calledWith(`
+Changes to package.json were detected.`));
+            assert(packageManager.adapter.log.getCall(1).calledWith('foo is not a supported package manager. Run it by yourself.'));
+          });
+
+          it('should not call spawnCommand', () => {
+            assert(packageManager.spawnCommand.notCalled);
+          });
+        });
+
+        describe('error detecting package manager', () => {
+          beforeEach(async () => {
+            packageManager.cwd = path.join(__dirname, 'fixtures', 'package-manager', 'npm');
+            packageManager.detectPackageManager = sinon.stub().returns(null);
+            await packageManager.packageManagerInstallTask();
+          });
+
+          it('should log', async () => {
+            assert(packageManager.adapter.log.calledTwice);
+            assert(packageManager.adapter.log.getCall(0).calledWith(`
+Changes to package.json were detected.`));
+            assert(packageManager.adapter.log.getCall(1).calledWith('Error detecting the package manager. Run it by yourself.'));
+          });
+
+          it('should not call spawnCommand', () => {
+            assert(packageManager.spawnCommand.notCalled);
+          });
+        });
+      });
+    });
+  });
+});
+/* eslint-enable max-nested-callbacks */
---
+++ test/spawn-command.js
@@ -0,0 +1,87 @@
+'use strict';
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+
+describe('environment (spawn-command)', () => {
+  let cwd;
+
+  beforeEach(function () {
+    this.spawnLib = sinon.stub();
+    this.spawnLib.sync = sinon.stub();
+    this.spawn = proxyquire('../lib/spawn-command', {
+      execa: this.spawnLib
+    });
+    cwd = Math.random().toString(36).slice(7);
+    this.spawn.cwd = cwd;
+  });
+
+  describe('#spawnCommand()', () => {
+    it('provide default options', function () {
+      this.spawn.spawnCommand('foo');
+      sinon.assert.calledWith(this.spawnLib, 'foo', undefined, {
+        cwd,
+        stdio: 'inherit'
+      });
+    });
+
+    it('pass arguments', function () {
+      this.spawn.spawnCommand('foo', 'bar');
+      sinon.assert.calledWith(this.spawnLib, 'foo', 'bar', {
+        cwd,
+        stdio: 'inherit'
+      });
+    });
+
+    it('pass options', function () {
+      this.spawn.spawnCommand('foo', undefined, {foo: 1});
+      sinon.assert.calledWith(this.spawnLib, 'foo', undefined, {
+        cwd,
+        foo: 1,
+        stdio: 'inherit'
+      });
+    });
+
+    it('allow overriding default options', function () {
+      this.spawn.spawnCommand('foo', undefined, {stdio: 'ignore'});
+      sinon.assert.calledWith(this.spawnLib, 'foo', undefined, {
+        cwd,
+        stdio: 'ignore'
+      });
+    });
+  });
+
+  describe('#spawnCommandSync()', () => {
+    it('provide default options', function () {
+      this.spawn.spawnCommandSync('foo');
+      sinon.assert.calledWith(this.spawnLib.sync, 'foo', undefined, {
+        cwd,
+        stdio: 'inherit'
+      });
+    });
+
+    it('pass arguments', function () {
+      this.spawn.spawnCommandSync('foo', 'bar');
+      sinon.assert.calledWith(this.spawnLib.sync, 'foo', 'bar', {
+        cwd,
+        stdio: 'inherit'
+      });
+    });
+
+    it('pass options', function () {
+      this.spawn.spawnCommandSync('foo', undefined, {foo: 1});
+      sinon.assert.calledWith(this.spawnLib.sync, 'foo', undefined, {
+        cwd,
+        foo: 1,
+        stdio: 'inherit'
+      });
+    });
+
+    it('allow overriding default options', function () {
+      this.spawn.spawnCommandSync('foo', undefined, {stdio: 'wut'});
+      sinon.assert.calledWith(this.spawnLib.sync, 'foo', undefined, {
+        cwd,
+        stdio: 'wut'
+      });
+    });
+  });
+});
