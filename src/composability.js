60de664d982a98b0ef7bec370ff3cea888379fb2
Marcelo Shima <marceloshima@gmail.com>
2023-05-17 11:28:25 -0300

    switch the requireGenerator to fly-import

--- src/composability.js
+++ src/composability.js
@@ -1,7 +1,8 @@
 import assert from 'node:assert';
+import { createHash } from 'node:crypto';
 import semver from 'semver';
 import pacote from 'pacote';
-import Generator from 'yeoman-generator';
+import { flyImport } from 'fly-import';
 
 /**
  * @mixin
@@ -13,12 +14,21 @@ export default composability;
 
 composability.requireGenerator = async function (namespace) {
   if (namespace === undefined) {
+    try {
+      const { default: Generator } = await import('yeoman-generator');
+      return Generator;
+    } catch {}
+
+    const { default: Generator } = await flyImport('yeoman-generator');
     return Generator;
   }
 
   // Namespace is a version
   if (semver.valid(namespace)) {
-    return this.repository.requireModule('yeoman-generator', namespace);
+    // Create a hash to install any version range in the local repository
+    const hash = createHash('shake256', { outputLength: 2 }).update(namespace, 'utf8').digest('hex');
+    const { default: Generator } = await flyImport(`@yeoman/generator-impl-${hash}@npm:yeoman-generator@${semver}`);
+    return Generator;
   }
 
   return this.get(namespace);
@@ -31,25 +41,14 @@ composability.requireGenerator = async function (namespace) {
  * @return  {Boolean} - true if the install succeeded.
  */
 composability.installLocalGenerators = async function (packages) {
-  const entries = Object.entries(packages).filter(
-    ([packageName, version]) => !this.repository.verifyInstalledVersion(packageName, version),
-  );
-  if (entries.length === 0) {
-    return true;
-  }
+  const entries = Object.entries(packages);
+  const specs = entries.map(([packageName, version]) => `${packageName}${version ? `@${version}` : ''}`);
+  await this.repository.install(specs);
 
-  const toInstall = {};
-  for (const [packageName, version] of entries) {
-    toInstall[packageName] = version;
-  }
+  const packagesToLookup = entries.map(([packageName, _]) => packageName);
+  await this.lookupLocalPackages(packagesToLookup);
 
-  if (await this.repository.installPackages(toInstall)) {
-    const packagesToLookup = entries.map(([packageName, _]) => packageName);
-    await this.lookupLocalPackages(packagesToLookup);
-    return true;
-  }
-
-  return false;
+  return true;
 };
 
 /**
