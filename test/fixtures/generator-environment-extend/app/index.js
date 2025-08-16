ab74502eac5dd6f14ac4f02fa0016344b541405e
Marcelo Shima <marceloshima@gmail.com>
2025-08-15 23:23:33 -0300

    chore: add node 22, 23, 24 to ci

--- test/fixtures/generator-environment-extend/app/index.js
+++ test/fixtures/generator-environment-extend/app/index.js
@@ -1,4 +1,4 @@
-import { lookupGenerator } from '../../../../src/generator-lookup.js';
+import { lookupGenerator } from '../../../../src/generator-lookup.ts';
 const maybeGenerator = await import(lookupGenerator('dummy:app'));
 const Generator = maybeGenerator.default ?? maybeGenerator;
 export default class extends Generator {};
